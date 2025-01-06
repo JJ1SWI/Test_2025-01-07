5076002777a446abdc4faaf7a19fe3507eec8c78
yumetake <yumetake@DESKTOP-2UU3K57>
2025-01-07 01:18:51 +0900

    コミットします。

--- Test_2025-01-07/Form1.cs
+++ Test_2025-01-07/Form1.cs
@@ -16,5 +16,10 @@ namespace Test_2025_01_07
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
